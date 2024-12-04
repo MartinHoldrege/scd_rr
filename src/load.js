d27a15c9c2e3ce18af1fb9617c3be792ce20b292
martinholdrege <martinholdrege@gmail.com>
2024-12-04 23:50:04 +0000

    Updated src/load.js (added 57 lines, deleted 10 lines).

--- src/load.js
+++ src/load.js
@@ -14,6 +14,7 @@ var f = require("users/MartinHoldrege/scd_rr:src/general_functions.js");
 
 var path = SEI.path;
 var pathPub = path + 'data_publication2/'; // SCD climate paper's data publication layers live here
+var pathRr = 'projects/ee-martinholdrege/assets/misc/newRR3/';
 
 // Dictionaries ---------------------------------------------------
 
@@ -31,7 +32,21 @@ var scenRrD = {
   'RCP45_2031-2060': '2029-2064-RCP45', // value is the naming scheme used in file name
   'RCP45_2071-2100': '2064-2099-RCP45',
   'RCP85_2031-2060': '2029-2064-RCP85',
-  'RCP85_2071-2100': '2064-2099-RCP85'
+  'RCP85_2071-2100': '2064-2099-RCP85',
+  'historical': '1980-2020-Ambient'
+};
+
+// components of the image names that correspond to the types of variables
+// note--be careful with these hand written dictionaries if the key/value 
+// pairs aren't correct the wrong layer will be shown! the key's 
+// are the names that will be shown in the drop-down menus
+var varRrD = {
+  'Resil-cats': ['Resil-cats', ''],
+  'Resil-cont': ['Resil-cont', ''],
+  'Resist-cats': ['Resist-cats', ''],
+  'Resist-cont': ['Resist-cont', ''],
+  'Resil-cont-delta': ['Resil-cont', '-delta'],
+  'Resist-cont-delta': ['Resist-cont', '-delta']
 };
 
 
@@ -44,9 +59,11 @@ var scenRrD = {
 var histSEI = ee.Image(pathPub + 'SEI-Q_v11_2017-2020')
   .rename('Q5s', 'Q1', 'Q2', 'Q3', 'Q4', 'Q5'); // Q5s is the continuous SEI
 
-exports.histC3 = SEI.seiToC3(histSEI.select('Q5s')); // historical 3 class SEI
+var histC3 = SEI.seiToC3(histSEI.select('Q5s'))
+  .rename('c3'); // historical 3 class SEI
 
-
+// returns image of with future (continuous) SEI
+// see getFutC3 for details on arguments
 var getFutSEI = function(args) {
   
   var scen = args.scen;
@@ -60,7 +77,7 @@ var getFutSEI = function(args) {
   
   // currently don't actually need to the the scenScdD dictionary
   // but if change input in the future it could be helpufl
-  var pathImage = pathPub + 'SEI_' + run + '_' + scenScD[scen];
+  var pathImage = pathPub + 'SEI_' + run + '_' + scenScdD[scen];
   var futSEI = ee.Image(pathImage)
     .rename(['SEI_low', 'SEI_high', 'SEI_median'])
     .select(summaries);
@@ -68,16 +85,46 @@ var getFutSEI = function(args) {
   return futSEI;
 };
 
-
-var getFutC3 = function(args) {
+/**
+ * return an image of future SEI classes
+ * @param  {object} args is a dictionary with these elements
+ *    scen: the scenario, string of form 'RCP_yyyy-yyyy' (see names of scenScdD for possible options, or 'historical')
+ *    run: optional string default is 'Default', (i.e. the modeling assumption)
+ *    summaries: opitional, list of summary bands to show (one or more of low, median, high, defaults to median only)
+ * dictionary element)
+ * @return image with 1-3 bands. 
+ */
+exports.getC3 = function(args) {
+  if(args.scen == 'historical') {
+    return histC3;
+  }
   var image = SEI.seiToC3(getFutSEI(args));
   return image;
 };
 
 
-
-
 // functions for loading rr assets -----------------------------------------------
 
+// return the name of the RR asset based on components of the name
+var createRrImageName = function(varName, scen) {
+  var varType = varRrD[varName];
+  var scenario = scenRrD[scen];
+  return varType[0] + '_' + scenario + varType[1];
+};
 
+
+/**
+ * return an image of R&R
+ * @param  {object} args is a dictionary with these elements
+ *    varName: the the variable name, one of the names in the varRrD object.
+ *    scen: the scenario of the the names in the scenRrD object. 
+ * @return image with 1 band
+ */
+var getRr = function(args) {
+    // form the image name
+    var imageName = createRrImageName(args.varName, args.scen); 
+    
+    var image = ee.Image(pathRr + imageName);
+    return image;
+};
 
